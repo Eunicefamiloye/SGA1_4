36fbb2cd7876aef375b9c7183e726183419677fe
Eunice Familoye <familoyeeunicebukunmi@gmail.com>
2022-10-19 22:13:04 +0100

    This is my first commit

---
+++ week1_day2/control_flow.py
@@ -0,0 +1,22 @@
+#if statement practice
+grade = 69
+if grade > 60
+      print('B')
+
+
+#if else practice
+grade = 70
+if grade > 69:
+    print('A')
+else:
+    print('B')
+
+#if elif and else practice
+grade = 70
+if grade > 70:
+    print('A')
+elif grade > 69 and < 60:
+    print('B')
+else:
+    print('Good but not excellent')
+
